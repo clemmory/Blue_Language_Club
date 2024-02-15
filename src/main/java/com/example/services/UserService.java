2e708a6364c6aa99e7029b06b12addf52ed0bcf2
clemmory <clementine.mory@gmail.com>
2024-02-15 17:42:38 +0100

    userService

--- src/main/java/com/example/services/UserService.java
+++ src/main/java/com/example/services/UserService.java
@@ -1,5 +1,21 @@
 package com.example.services;
 
+import java.util.List;
+
+import com.example.entities.User;
+
 public interface UserService {
+
+    //Créer un participant
+    public User createUser(User user);
+
+    //Afficher un participant grace a son id
+    public User findByUser(int idUser);
+
+    //Supprimer un participant
+    public void deleteUser(User user);
+
+    //Afficher tous les participants
+    public List<User> findAllUsers();
 
 }
