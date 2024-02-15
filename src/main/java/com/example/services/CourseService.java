82443cd0978a6f1885645ae84b229fb8dfb5ea76
dapopadev <danielapopa1411@gmail.com>
2024-02-15 17:41:48 +0100

    CourseService

--- src/main/java/com/example/services/CourseService.java
+++ src/main/java/com/example/services/CourseService.java
@@ -1,6 +1,20 @@
 package com.example.services;
 
+import java.util.List;
+
+import com.example.entities.Course;
+
 public interface CourseService {
-  
+
+    // Créer un cours
+    public Course saveCourse (Course course);
+    //Supprimer un cours
+    public void deleteCourse(Course course);
+    //Afficher liste des cours
+    public List<Course> findAllCourses();
+    //Afficher un cours par Id
+    public Course findByIdCourse(int id);
+    
+    
 
 }
--- src/main/java/com/example/services/CourseServiceImpl.java
+++ src/main/java/com/example/services/CourseServiceImpl.java
@@ -1,6 +1,14 @@
 package com.example.services;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import com.example.dao.CourseDao;
+import com.example.entities.Course;
 
 import lombok.RequiredArgsConstructor;
 
@@ -8,6 +16,28 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 
  public class CourseServiceImpl implements CourseService {
+    
+    private final CourseDao courseDao;
 
+    @Override
+    public Course saveCourse(Course course) {
+        return courseDao.save(course);
+    }
+
+    @Override
+    public void deleteCourse(Course course) {
+       courseDao.delete(course);
+    }
+
+    @Override
+    public List<Course> findAllCourses() {
+      return courseDao.findAll();
+    }
+
+    @Override
+    public Course findByIdCourse(int id) {
+        return courseDao.findById(id).get();
+    }
+    
 
  }
