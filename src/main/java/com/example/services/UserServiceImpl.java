55248a2df8c013934eef6e86af8c2ee3cc1ea724
dapopadev <danielapopa1411@gmail.com>
2024-02-15 17:50:04 +0100

    test

--- src/main/java/com/example/services/UserServiceImpl.java
+++ src/main/java/com/example/services/UserServiceImpl.java
@@ -2,10 +2,7 @@ package com.example.services;
 
 import org.springframework.stereotype.Service;
 
-import lombok.RequiredArgsConstructor;
-
 @Service
-@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
 }
