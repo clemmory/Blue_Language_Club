bc51974c0c6e9b1c25a2cb544e8f5d2cbdf25fe5
clemmory <clementine.mory@gmail.com>
2024-02-16 13:59:55 +0100

    test

--- src/main/java/com/example/entities/User.java
+++ src/main/java/com/example/entities/User.java
@@ -7,12 +7,15 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -33,11 +36,23 @@ public class User implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
+    @NotBlank(message = "Please enter your firstname.")
     private String firstName;
+
+    @NotBlank(message = "Please enter your surname.")
     private String surname;
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int global_id;
+    
     private String email;
+
+    @Enumerated
+    @NotNull(message = "Please indicate the language you want to improve.")
     private LANGUAGE language;
+
+    @Enumerated
+    @NotNull(message = "Please indicate your current level.")
     private LEVEL initial_level;
 
     // Ajouter la relation MANY to MANY avec users
