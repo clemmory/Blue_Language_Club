a585c97b9e2f39b38e34e062e4699df68ebd7bf2
clemmory <clementine.mory@gmail.com>
2024-02-22 17:48:44 +0100

    Branch controllers with method delete by Id

--- src/main/java/com/blueLanguageClub/controllers/CourseController.java
+++ src/main/java/com/blueLanguageClub/controllers/CourseController.java
@@ -11,7 +11,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -81,6 +83,30 @@ public class CourseController {
 
         return responseEntity;
 
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Map<String, Object>> deleteCourseById(@PathVariable(name = "id", required = true) Integer idCourse) {
+
+        Map<String, Object> responseAsMap = new HashMap<>();
+        ResponseEntity<Map<String, Object>> responseEntity = null;
+
+        try {
+
+            courseService.deleteCourse(courseService.findByIdCourse(idCourse));
+            String successMessage = "The course with id " + idCourse + " has been deleted.";
+            responseAsMap.put("successMessage", successMessage);
+            responseEntity = new ResponseEntity<>(responseAsMap, HttpStatus.OK);
+
+        } catch (DataAccessException e) {
+
+            String seriousError = "An error occurred while deleting the product with id " + idCourse + ", and the most probable cause is " + e.getMostSpecificCause();
+            responseAsMap.put("seriousError", seriousError);
+            responseEntity = new ResponseEntity<Map<String,Object>>(responseAsMap, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        return responseEntity;
     }
 
 }
