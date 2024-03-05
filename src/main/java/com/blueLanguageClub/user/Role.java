31012e71fef744ee1af2eeb53c212513e7d31319
dapopadev <danielapopa1411@gmail.com>
2024-03-05 10:08:09 +0100

    security sin JWT

---
+++ src/main/java/com/blueLanguageClub/user/Role.java
@@ -0,0 +1,5 @@
+package com.blueLanguageClub.user;
+
+public enum Role {
+    ADMIN, USER
+}
---
+++ src/main/java/com/blueLanguageClub/user/UserService.java
@@ -0,0 +1,11 @@
+package com.blueLanguageClub.user;
+
+import java.util.List;
+
+public interface UserService {
+    public abstract User add(User user);
+    public abstract List<User> findAll();
+    public abstract void deleteByEmail(String email);
+    public abstract User findByEmail(String email);
+    public abstract User update(User user);    
+}
